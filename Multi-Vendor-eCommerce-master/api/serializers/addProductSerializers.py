d0eb45f76c1327a254da6aff97ab276e7e821827
wajdiJomaa <blackwajdi9@gmail.com>
2023-06-16 13:54:19 +0300

    adding api routes

--- Multi-Vendor-eCommerce-master/api/serializers/addProductSerializers.py
+++ Multi-Vendor-eCommerce-master/api/serializers/addProductSerializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from product.models import Product, CategoryProductOptions, Category, CategoryOptions
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -14,31 +12,25 @@ class CategoryOptionsSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class CategoryProductOptionsSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CategoryProductOptions
-        fields = ["category_option", "value"]
+        fields = ["product","category_option", "value"]
 
-    def create(self, validated_data):
-        product = self.context['product_id']
-        product_option = CategoryProductOptions.objects.create(product=product, **validated_data)
-
-        return product_option
 
 class AddProductSerializer(serializers.ModelSerializer):
-    category_options = CategoryProductOptionsSerializer(many=True, write_only=True)
-
+    # category_options = CategoryProductOptionsSerializer(many=True, write_only=True)
     class Meta:
         model = Product
-        fields = ["title", "description", "price", "image", "countInStock","created_by", "category", "category_options"]
+        fields = ["id","title", "description", "price", "image", "countInStock","created_by", "category"]
+                  # "category_options"]
 
-    def create(self, validated_data):
-        options_data = validated_data.pop('category_options')
-        product = Product.objects.create(**validated_data)
-
-        for option_data in options_data:
-            product_option_serializer = CategoryProductOptionsSerializer(data=option_data, context={'product_id': product})
-            if product_option_serializer.is_valid():
-                product_option_serializer.save()
-
-        return product
+    # def create(self, validated_data):
+    #     options_data = validated_data.pop('category_options')
+    #     product = Product.objects.create(**validated_data)
+    #
+    #     # for option_data in options_data:
+    #     #     product_option_serializer = CategoryProductOptionsSerializer(data=option_data, context={'product_id': product})
+    #     #     if product_option_serializer.is_valid():
+    #     #         product_option_serializer.save()
+    #
+    #     return product
--- Multi-Vendor-eCommerce-master/api/views.py
+++ Multi-Vendor-eCommerce-master/api/views.py
@@ -3,14 +3,17 @@ import sys
 from os.path import dirname, abspath
 sys.path.append(dirname(dirname(abspath(__file__))))
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers.customerSerializers import CustomerSerializer
-from .serializers.productSerializers import ProductSimpleSerializer, ProductDetailsSerializer
+from .serializers.productSerializers import *
 from .serializers.addProductSerializers import *
-
+from rest_framework import status
 from customers.models import Customer
-from product.models import Product, Category, CategoryOptions
+from product.models import Product, Category, CategoryOptions, CategoryProductOptions
 
 # Create your views here.
 class RegisterCustomerView(generics.CreateAPIView):
@@ -28,6 +31,15 @@ class GetProductsView(generics.ListAPIView):
     queryset = Product.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = ProductSimpleSerializer
+
+
+class GetVendorProductsView(generics.ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ProductSimpleSerializer
+
+    def get_queryset(self):
+        vendor_id = self.kwargs["vendor_id"]
+        return Product.objects.filter(created_by=vendor_id)
 
 class GetProductDetails(generics.ListAPIView):
     permission_classes = (AllowAny,)
@@ -49,10 +61,19 @@ class GetCategoryOptions(generics.ListAPIView):
 
     def get_queryset(self):
         category_id = self.kwargs["category_id"]
-        return CategoryOptions.objects.filter(id=category_id)
+        return CategoryOptions.objects.filter(category=category_id)
 
 
 class AddCategoryOption(generics.CreateAPIView):
-    queryset = CategoryOptions  .objects.all()
+    queryset = CategoryOptions.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = CategoryOptionsSerializer
+
+class AddCategoryProductOptions(APIView):
+    def post(self, request, format=None):
+        serializer = CategoryProductOptionsSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
